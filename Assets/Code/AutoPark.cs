e393f47a0774b2c256aa7276c6d4f190c7b8f4ae
Andreas Kielsgaard <andreas.kielsgaard.code@gmail.com>
2016-09-19 15:59:05 +0200

    Made a v2 of the shift algorithm
    Makes a 4 point turn driving forward first two points, and backwards the
    two last, ending up in the same position relative to the z coordinate

--- Assets/Code/AutoPark.cs
+++ Assets/Code/AutoPark.cs
@@ -7,8 +7,8 @@ public class AutoPark : MonoBehaviour{
 	public ControlCar control;
 
 	float rrad = Mathf.PI * 2;
-
 	int checkCount = 0;
+	bool doingSubRoutine = false;
 
 	void Start()
 	{
@@ -38,13 +38,15 @@ public class AutoPark : MonoBehaviour{
 		float backSensorOffset = 0.1f;
 		float angle = 0;
 
-		//Drive back
+		//Drive back without turning
 		control.command = new ControlCar.CommandSet(-1, 0, false); 
 		while (backPointingRight.getDistance() != -1) yield return null; // until back end of car is aligned with obstacle 1
-		breaK();
-		turnRight();
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
 
-		//Drive back while turning
+		StartCoroutine(turnRight());
+		while (doingSubRoutine) yield return null;
+		//Drive back while turning right
 		control.command = new ControlCar.CommandSet(-1, 1, false); 
 		while (backMiddlePointingBack1.getDistance() == -1) yield return null; //until obstacle two is visible;
 		while (backMiddlePointingBack1.getDistance() != -1) { //and then until it isn't visible again, while keeping track of the last measured value
@@ -56,24 +58,27 @@ public class AutoPark : MonoBehaviour{
 		// Calculate angle between alignent of obstacle2(and supposedly object1) and the alignment of the car
 		angle = Mathf.Atan((lastDistance1 - lastDistance2) / backSensorOffset) * Mathf.Rad2Deg;
 		//Set the rotation of the angluar distance sensor to the difference in alignment
-		frontRightAngular.transform.localEulerAngles = new Vector3(0, 180 + angle, 0); 
+		frontRightAngular.transform.localEulerAngles = new Vector3(0, 180 + angle, 0);
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
 
-		breaK();
-		turnMiddle();
-
-		//Drive back
+		StartCoroutine(turnMiddle());
+		while (doingSubRoutine) yield return null;
+		//Drive back without turning
 		control.command = new ControlCar.CommandSet(-1, 0, false); 
 		while (frontRightAngular.getDistance() == -1) yield return null; //Until front right corner of car clears obstacle 1
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
 
-		breaK();
-		turnLeft();
-
-		//Drive back while turning
+		StartCoroutine(turnLeft());
+		while (doingSubRoutine) yield return null;
+		//Drive back while turning left
 		control.command = new ControlCar.CommandSet(-1, -1, false);             // until car is aligned with object 2
 		while (Mathf.Atan((backMiddlePointingBack1.getDistance() - backMiddlePointingBack2.getDistance()) / backSensorOffset) * Mathf.Rad2Deg > 0 || backMiddlePointingBack1.getDistance() == -1) yield return null; ;
-																	
-		breaK();
-		turnMiddle();
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+		StartCoroutine(turnMiddle());
+		while (doingSubRoutine) yield return null;
 
 		//Done - Give back control
 		control.autoParking = false;
@@ -87,7 +92,7 @@ public class AutoPark : MonoBehaviour{
 
 	public void doShiftDistance(float distance)
 	{
-		StartCoroutine( shiftDistance(5));
+		StartCoroutine( shiftDistanceV2(20));
 	}
 	private IEnumerator shiftDistance(float distance)
 	{
@@ -108,68 +113,182 @@ public class AutoPark : MonoBehaviour{
 			extraDistance = distance - (control.ib60TurnR + control.ob60TurnR);
 		}
 		else angle = Mathf.Acos(1 - (distance / (control.ib60TurnR + control.ob60TurnR)));
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
 
-		breaK();
-		turnRight();
-
-		//Back while turning
+		StartCoroutine(turnRight());
+		while (doingSubRoutine) yield return null;
+		//Back while turning right
 		control.command = new ControlCar.CommandSet(-1, 1, false); 
 		float a1 = (startAngle - angle + rrad) % rrad + 0.01f;
 		float a2 = (startAngle - angle + rrad) % rrad - 0.01f;
 		if ((startAngle - angle + rrad) % rrad < 0.01f) a2 = 0;
 		if ((startAngle - angle + rrad) % rrad > rrad - 0.01f) a1 = rrad;
 		while (control.getBodyAngleR() > a1 || control.getBodyAngleR() < a2) yield return null; //Until angle is more than required angle
-		breaK();
-		
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+
 		//If staight backing is necessary, do it now
 		if (turnRadiusExceeded){ //If staight backing is necessary, do it now
-			backUpDistance(extraDistance);
-			breaK();
+			StartCoroutine(backUpDistance(extraDistance));
+			while (doingSubRoutine) yield return null;
+			StartCoroutine(breaK());
+			while (doingSubRoutine) yield return null;
 		}
-		turnLeft();
 
-		//Drive back while turning
+		StartCoroutine(turnLeft());
+		while (doingSubRoutine) yield return null;
+		//Drive back while turning left
 		control.command = new ControlCar.CommandSet(-1, -1, false); 
 		a1 = startAngle + 0.01f;
 		a2 = startAngle - 0.01f;
 		if (startAngle < 0.01f) a2 = 0;
 		if (startAngle > rrad - 0.01f) a1 = rrad;
 		while (control.getBodyAngleR() > a1 || control.getBodyAngleR() < a2 % (2 * Mathf.PI)) yield return null; //until back at start angle
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+		StartCoroutine(turnMiddle());
+		while (doingSubRoutine) yield return null;
 
-		breaK();
-		turnMiddle();
-		
 		//Done - Give back control
 		control.autoParking = false; 
+	}
+	private IEnumerator shiftDistanceV2(float distance)
+	{
+
+		//Enable automatic control
+		control.autoParking = true;
+
+		//Prepare variables
+		bool turnRadiusExceeded = false;
+		float extraDistance = 0;
+		float angle = 0;
+		float startAngle = control.getBodyAngleR();
+
+		//Check if straight driving is needed and prepare variables
+		if (distance > (control.ib60TurnR + control.ob60TurnR) * 2)
+		{
+			Debug.Log("Straight needed");
+			angle = 90 * Mathf.Deg2Rad;
+			turnRadiusExceeded = true;
+			extraDistance = distance - (control.ib60TurnR + control.ob60TurnR) * 2;
+		}
+		else angle = Mathf.Acos(1 - (distance / ((control.ib60TurnR + control.ob60TurnR) * 2)));
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+
+
+		StartCoroutine(turnRight());
+		//Drive forward while turning right
+		control.command = new ControlCar.CommandSet(1, 1, false);
+		float a1 = (startAngle + angle + rrad) % rrad + 0.01f;
+		float a2 = (startAngle + angle + rrad) % rrad - 0.01f;
+		if ((startAngle + angle + rrad) % rrad < 0.01f) a2 = 0;
+		if ((startAngle + angle + rrad) % rrad > rrad - 0.01f) a1 = rrad;
+		while (control.getBodyAngleR() > a1 || control.getBodyAngleR() < a2) yield return null; //Until angle is more than required angle
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+
+		//If staight driving is necessary, do it now
+		if (turnRadiusExceeded){
+			Debug.Log("Driving straight");
+			StartCoroutine( driveDistance(extraDistance));
+			while (doingSubRoutine) yield return null;
+			StartCoroutine(breaK());
+			while (doingSubRoutine) yield return null;
+		}
+
+		StartCoroutine(turnLeft());
+		while (doingSubRoutine) yield return null;
+		//Drive forward while turning left
+		control.command = new ControlCar.CommandSet(1, -1, false);
+		a1 = startAngle + 0.01f;
+		a2 = startAngle - 0.01f;
+		if (startAngle < 0.01f) a2 = 0;
+		if (startAngle > rrad - 0.01f) a1 = rrad;
+		while (control.getBodyAngleR() > a1 || control.getBodyAngleR() < a2 % (2 * Mathf.PI)) yield return null; //until back at start angle
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+
+		StartCoroutine(turnRight());
+		while (doingSubRoutine) yield return null;
+		//Back while turning right
+		control.command = new ControlCar.CommandSet(-1, 1, false);
+		a1 = (startAngle - angle + rrad) % rrad + 0.01f;
+		a2 = (startAngle - angle + rrad) % rrad - 0.01f;
+		if ((startAngle - angle + rrad) % rrad < 0.01f) a2 = 0;
+		if ((startAngle - angle + rrad) % rrad > rrad - 0.01f) a1 = rrad;
+		while (control.getBodyAngleR() > a1 || control.getBodyAngleR() < a2) yield return null; //Until angle is more than required angle
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+
+		StartCoroutine(turnLeft());
+		while (doingSubRoutine) yield return null;
+		//Drive back while turning left
+		control.command = new ControlCar.CommandSet(-1, -1, false);
+		a1 = startAngle + 0.01f;
+		a2 = startAngle - 0.01f;
+		if (startAngle < 0.01f) a2 = 0;
+		if (startAngle > rrad - 0.01f) a1 = rrad;
+		while (control.getBodyAngleR() > a1 || control.getBodyAngleR() < a2 % (2 * Mathf.PI)) yield return null; //until back at start angle
+
+		StartCoroutine(breaK());
+		while (doingSubRoutine) yield return null;
+		StartCoroutine(turnMiddle());
+		while (doingSubRoutine) yield return null;
+
+		//Done - Give back control
+		control.autoParking = false;
 
 	}
 
-	private IEnumerable breaK()
+	private IEnumerator breaK()
 	{
+		doingSubRoutine = true;
 		control.command = new ControlCar.CommandSet(0, 0, true); //Break
 		while (control.getVelocity() > 0.5f) yield return null; // until allmost at standstill
+		doingSubRoutine = false;
 	}
-	private IEnumerable turnLeft()
+	private IEnumerator turnLeft()
 	{
+		doingSubRoutine = true;
 		control.command = new ControlCar.CommandSet(0, -1, false); //Turn to the left
 		while (!(control.getWheelAngle() > (360 - control.maxRotation - 1) && control.getWheelAngle() < (360 - control.maxRotation + 1))) yield return null; // until wheel is allmost fully turned
+		doingSubRoutine = false;
 	}
-	private IEnumerable turnMiddle()
+	private IEnumerator turnMiddle()
 	{
+		doingSubRoutine = true;
 		control.command = new ControlCar.CommandSet(0, 1, false); //Turn to middle position
-		while (control.getWheelAngle() > 100 && control.getWheelAngle() < 356) yield return null; // until done
+		while (!(control.getWheelAngle() < 3 || control.getWheelAngle() > 357)) yield return null; // until done
+		doingSubRoutine = false;
 	}
-	private IEnumerable turnRight()
+	private IEnumerator turnRight()
 	{
+		doingSubRoutine = true;
 		control.command = new ControlCar.CommandSet(0, 1, false); //Turn to the right
 		while (!(control.getWheelAngle() > control.maxRotation - 1 && control.getWheelAngle() < control.maxRotation + 1)) yield return null; // until wheel is allmost fully turned
+		doingSubRoutine = false;
 	}
-	private IEnumerable backUpDistance(float distance)
+	private IEnumerator backUpDistance(float distance)
 	{
+		doingSubRoutine = true;
 		Vector3 pos = control.getPosition();
-		turnMiddle();
+		control.command = new ControlCar.CommandSet(0, 1, false); //Turn to middle position
+		while (control.getWheelAngle() > 100 && control.getWheelAngle() < 356) yield return null; // until done
 		control.command = new ControlCar.CommandSet(-1, 0, false); //Back up
 		while (control.distanceToPoint(pos) < distance) yield return null; // until an appropriate distance from starting point
+		doingSubRoutine = false;
+	}
+	private IEnumerator driveDistance(float distance)
+	{
+		doingSubRoutine = true;
+		Vector3 pos = control.getPosition();
+		control.command = new ControlCar.CommandSet(0, -1, false); //Turn to middle position
+		while (!(control.getWheelAngle() < 3 || control.getWheelAngle() > 357)) yield return null; // until done
+		control.command = new ControlCar.CommandSet(1, 0, false); //Drive ahead
+		while (control.distanceToPoint(pos) < distance) yield return null; // until an appropriate distance from starting point
+		doingSubRoutine = false;
 	}
 
 
